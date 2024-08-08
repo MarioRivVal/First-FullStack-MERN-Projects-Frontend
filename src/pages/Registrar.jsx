1becd4920217467c58ea808c2a6d527042f1ce65
NETNEO <rival.moro@hotmail.it>
2024-08-08 19:41:44 +0200

    conecting with backend and register the frst user

--- src/pages/Registrar.jsx
+++ src/pages/Registrar.jsx
@@ -1,6 +1,48 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
+import Alerta from "../components/Alerta";
 
 const registrar = () => {
+  const [nombre, setNombre] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repetirPassword, setRepetirPassword] = useState("");
+
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ([nombre, email, password, repetirPassword].includes("")) {
+      setAlerta({ msg: "hay campos vacios", error: true });
+      return;
+    }
+
+    if (password !== repetirPassword) {
+      setAlerta({ msg: "passwords diferentes", error: true });
+      return;
+    }
+    if (password.length < 6) {
+      setAlerta({
+        msg: "passwords muy corto, agrega almenos 6 caracteres",
+        error: true,
+      });
+      return;
+    }
+    setAlerta({});
+
+    // CREAR LOS REGISTROS EN LA API
+    try {
+      const url = "http://localhost:4000/api/veterinarios";
+      await axios.post(url, { nombre, email, password });
+      setAlerta({ msg: "Creado Correctamente, revisa tu email", error: false });
+    } catch (error) {
+      setAlerta({ msg: error.response.data.msg, error: true });
+    }
+  };
+
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -11,7 +53,9 @@ const registrar = () => {
       </div>
 
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+        {msg && <Alerta alerta={alerta} />}
+
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">
               Nombre
@@ -20,6 +64,8 @@ const registrar = () => {
               type="text"
               placeholder="Tu nombre"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -30,6 +76,8 @@ const registrar = () => {
               type="email"
               placeholder="Email de Registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -40,6 +88,8 @@ const registrar = () => {
               type="password"
               placeholder="Tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className="my-5">
@@ -50,6 +100,8 @@ const registrar = () => {
               type="password"
               placeholder="Repite tu Password"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={repetirPassword}
+              onChange={(e) => setRepetirPassword(e.target.value)}
             />
           </div>
           <input
