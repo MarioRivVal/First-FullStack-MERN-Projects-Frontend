59914772f696b74c34e7a4760792311564860dd6
NETNEO <rival.moro@hotmail.it>
2024-08-14 19:42:49 +0200

    context creation and NuevoPasword functionlity

--- src/pages/OlvidePassword.jsx
+++ src/pages/OlvidePassword.jsx
@@ -1,6 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Alerta from "../components/Alerta";
+import clienteAxios from "../config/axios";
 
 const olvidePassword = () => {
+  const [email, setEmail] = useState("");
+  const [alerta, setAlerta] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (email === "" || email.length < 6) {
+      setAlerta({
+        msg: "El email es obligatorio",
+        error: true,
+      });
+      return;
+    }
+
+    try {
+      const data = await clienteAxios.post("/veterinarios/olvide-password", {
+        email,
+      });
+      console.log(data);
+
+      setAlerta({ msg: data.data.msg });
+    } catch (error) {
+      setAlerta({
+        msg: error.response.data.msg,
+        error: true,
+      });
+    }
+  };
+  const { msg } = alerta;
+
   return (
     <>
       <div>
@@ -10,7 +43,8 @@ const olvidePassword = () => {
         </h1>
       </div>
       <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
-        <form>
+        {msg && <Alerta alerta={alerta} />}
+        <form onSubmit={handleSubmit}>
           <div className="my-5">
             <label className="uppercase text-gray-600 block text-xl font-bold">
               Tu email
@@ -19,6 +53,8 @@ const olvidePassword = () => {
               type="email"
               placeholder="Email de registro"
               className="border w-full p-3 mt-3 bg-gray-50 rounded-xl"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
           <input
